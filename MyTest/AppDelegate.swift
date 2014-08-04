cec5f53f68af7d39675724a899d7269a61be6b49
ay65535 <ay65535@icloud.com>
2014-08-04 18:42:13 +0900

    テキストファイルからとりあえず読み込めるようになった。

--- MyTest/AppDelegate.swift
+++ MyTest/AppDelegate.swift
@@ -9,18 +9,36 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-                            
+    
     @IBOutlet var window: NSWindow
-
-
+    @IBOutlet var txtField: NSTextField
+    @IBOutlet var scrollView: NSScrollView
+    
+    var txtView : NSTextView {
+    get {
+        return scrollView.contentView.documentView as NSTextView
+    }
+    }
+    
+    var mytest: MyTest!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
+        mytest = MyTest()
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @IBAction func btnClicked(sender: AnyObject) {
+        var text = mytest.get()
+        update(text)
+    }
+    
+    func update(text: String) {
+        txtView.insertText(text)
+    }
+    
 }
 
